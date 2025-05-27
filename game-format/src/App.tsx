14dca8a3ca84d3df053b34b984612d612d515184
xinnnyeee <155549286+xinnnyeee@users.noreply.github.com>
2025-05-27 09:47:07 +0800

    Set up basic framework and the landing page

--- game-format/src/App.tsx
+++ game-format/src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// src/App.tsx
+import "./index.css"; // Make sure this line exists
+import { Routes, Route } from "react-router-dom";
+import Landing from "./Landing";
+import InputPage from "./pages/InputPage";
+import GamePage from "./pages/GamePage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/input" element={<InputPage />} />
+      <Route path="/pages/GamePage" element={<GamePage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
