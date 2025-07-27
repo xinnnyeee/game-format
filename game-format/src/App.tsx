d9d79b0977157359c22c7e2689313a218bd9676f
xinnnyeee <155549286+xinnnyeee@users.noreply.github.com>
2025-07-27 22:29:55 +0800

    Add Browser wrapper

--- game-format/src/App.tsx
+++ game-format/src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import "./index.css"; // Make sure this line exists
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import InputPage from "./pages/InputPage";
 import RRGamePage from "./pages/RRGamePage";
@@ -13,26 +13,28 @@ import KOTCGameSummary from "./pages/KOTCGameSummary";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/InputPage" element={<InputPage />} />
-      <Route path="/InputPage/RRGamePage" element={<RRGamePage />} />
-      <Route path="/InputPage/SKGamePage" element={<SKGamePage />} />
-      <Route path="/InputPage/OPGamePage" element={<OPGamePage />} />
-      <Route path="/InputPage/KOTCGamePage" element={<KOTCGamePage />} />
-      <Route
-        path="/InputPage/RRGamePage/RRGameSummary"
-        element={<RRGameSummary />}
-      />
-      <Route
-        path="/InputPage/SKGamePage/SKGameSummary"
-        element={<SKGameSummary />}
-      />
-      <Route
-        path="/InputPage/KOTCGamePage/KOTCGameSummary"
-        element={<KOTCGameSummary />}
-      />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/InputPage" element={<InputPage />} />
+        <Route path="/InputPage/RRGamePage" element={<RRGamePage />} />
+        <Route path="/InputPage/SKGamePage" element={<SKGamePage />} />
+        <Route path="/InputPage/OPGamePage" element={<OPGamePage />} />
+        <Route path="/InputPage/KOTCGamePage" element={<KOTCGamePage />} />
+        <Route
+          path="/InputPage/RRGamePage/RRGameSummary"
+          element={<RRGameSummary />}
+        />
+        <Route
+          path="/InputPage/SKGamePage/SKGameSummary"
+          element={<SKGameSummary />}
+        />
+        <Route
+          path="/InputPage/KOTCGamePage/KOTCGameSummary"
+          element={<KOTCGameSummary />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
